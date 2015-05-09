cbbc36554cdd9ae3256fb4f4d7e994cf4c9f340f
saeful rizky <saeful rizky@saefulrizky>
2015-05-09 08:14:41 +0700

    Test case pangkat

---
+++ src/main/java/TstsRunner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author saeful rizky
+ */
+public class TstsRunner {
+
+    public static void main(String[] args) {
+        testing test = new testing();
+        test.awalTest();
+        test.testPangkat1();
+        test.testPangkat2();
+    }
+}
---
+++ src/main/java/testing.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author saeful rizky
+ */
+public class testing {
+
+    private static Soal s;
+
+    @Before
+    public void awalTest() {
+        s = new Soal();
+        System.out.println("Mengawali testing");
+    }
+
+    @Test
+    public void testPangkat1() {
+        assertSame("Harusnya ", 4, s.pangkat(2, 2));
+    }
+
+    @Test
+    public void testPangkat2() {
+        assertNotSame("Harusnya beda ", 2, s.pangkat(2, 2));
+    }
+
+}
